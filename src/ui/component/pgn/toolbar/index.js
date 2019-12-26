0d6f349f2287d94cb33f4fde5353f1245d1e0830
Miguel Espinoza <wiseit@outlook.com>
2019-12-26 23:53:56 +0100

    Embryo clipboard

--- src/ui/component/pgn/toolbar/index.js
+++ src/ui/component/pgn/toolbar/index.js
@@ -1,7 +1,14 @@
 const m = require('mithril')
+const clipboard = require('./clipboard')
 
 const pgn_toolbar = {
-    view: () => m('div', 'pgn-toolbar')
+    view: () => [
+        m('button.toolbar-item', {
+            onclick: e => clipboard.show()
+            , title: 'Copy to and from Clipboard'
+        }, 'Clipboard'),
+        m(clipboard)
+    ]
 }
 
 module.exports = pgn_toolbar
