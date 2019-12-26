97faebe807c63db762ef5e4ddaa2b0058d4cc3b6
Miguel Espinoza <wiseit@outlook.com>
2019-12-26 23:47:40 +0100

    Board buttons backwards and forwards

--- src/ui/component/board/toolbar/index.js
+++ src/ui/component/board/toolbar/index.js
@@ -1,14 +1,33 @@
 const m = require('mithril')
 const Board = require('../chessground')
+const Moves = require('../../pgn/moves')
 require('./index.scss')
 const toolbar = {
     view: () => [
-        m('button', { onclick: () => toggle_orientation() }, 'Flip')
+        m('button.toolbar-item', { onclick: () => move_backwards() }, '<'),
+        m('button.toolbar-item', { onclick: () => toggle_orientation() }, 'Flip'),
+        m('button.toolbar-item', { onclick: () => move_forwards() }, '>')
     ]
 }
 
 function toggle_orientation() {
     Board.chessground.toggleOrientation()
     localStorage.setItem('board.orientation', Board.chessground.state.orientation)
+}
+
+function move_backwards() {
+    if (Moves.move_list.move_backward()) {
+        Board.chessjs.load(Moves.move_list.current_fen)
+        Board.sync()
+        Moves.move_list.update_vnodes()
+    }
+}
+
+function move_forwards() {
+    if (Moves.move_list.move_forward()) {
+        Board.chessjs.load(Moves.move_list.current_fen)
+        Board.sync()
+        Moves.move_list.update_vnodes()
+    }
 }
 module.exports = toolbar
