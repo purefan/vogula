387afd882855574eafe4ab2c925c2edbd75e4780
Miguel Espinoza <wiseit@outlook.com>
2020-01-06 13:26:31 +0100

    listen to arrow presses fwd and bkd

--- src/ui/component/board/toolbar/index.js
+++ src/ui/component/board/toolbar/index.js
@@ -2,6 +2,17 @@ const m = require('mithril')
 const Board = require('../chessground')
 const Moves = require('../../pgn/moves')
 const toolbar = {
+    oninit: () => {
+        document.onkeydown = e => {
+            e.preventDefault()
+            if (e.keyCode == 39) {
+                move_forwards()
+            }
+            if (e.keyCode == 37) {
+                move_backwards()
+            }
+        }
+    },
     view: () => [
         m('button.toolbar-item', { onclick: () => move_backwards() }, '<'),
         m('button.toolbar-item', { onclick: () => toggle_orientation() }, 'Flip'),
@@ -14,6 +25,10 @@ function toggle_orientation() {
     localStorage.setItem('board.orientation', Board.chessground.state.orientation)
 }
 
+/**
+ * Moves backwards one move and syncs the chessground configuration
+ * it is triggered by pressing a key or clicking on a button
+ */
 function move_backwards() {
     if (Moves.move_list.move_backward()) {
         Board.chessjs.load(Moves.move_list.current_fen)
@@ -22,6 +37,10 @@ function move_backwards() {
     }
 }
 
+/**
+ * Moves forwards one move and syncs the chessground configuration
+ * it is triggered by pressing a key or clicking on a button
+ */
 function move_forwards() {
     if (Moves.move_list.move_forward()) {
         Board.chessjs.load(Moves.move_list.current_fen)
